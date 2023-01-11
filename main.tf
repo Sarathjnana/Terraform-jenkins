44c0ef47d9d75dab58e2ef1e1b4f446c64c3b75b
Sarathjnana <119930337+Sarathjnana@users.noreply.github.com>
2023-01-11 11:07:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,20 +15,15 @@ provider "aws" {
 
 # Creating nginx_server 
 
-#resource "aws_instance" "nginx_server" {
-#  ami           = "ami-0f69bc5520884278e"
- # instance_type = "t2.micro"
-  #  availability_zone = "ap-south-1"
- # user_data = file("apply.sh")
+resource "aws_instance" "nginx_server" {
+  ami           = "ami-0f69bc5520884278e"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1"
+  user_data = file("apply.sh")
 
- # tags = {
-#    Name = "nginx_ser"
-#  }
-#}
-
-#Creating IAM user
-
-resource "aws_iam_user" "user" {
-  name = "test-user"
+  tags = {
+    Name = "nginx_ser"
+  }
 }
+
 
